f8f440eaa0fe1cafe847cac2dac325a876d5d38c
quadflal <alexquadf@gmail.com>
2025-10-27 21:52:40 +0100

    Initial commit

---
+++ src/test/java/de/quadflal/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package de.quadflal;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
